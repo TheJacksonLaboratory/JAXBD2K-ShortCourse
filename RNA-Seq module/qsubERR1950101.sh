ae511eea56993e8a477f76b8f50d10c2d78158f8
Y. Ada Zhan <zyqfrog10@gmail.com>
2018-10-05 15:46:41 -0400

    Update qsubERR1950101.sh

--- RNA-Seq module/qsubERR1950101.sh
+++ RNA-Seq module/qsubERR1950101.sh
@@ -5,12 +5,8 @@ chop=./chop.sh
 genomedir=./mm10_star
 
 # mm10 gene annotation
-wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M16/gencode.vM16.annotation.gtf.gz
-gunzip gencode.vM16.annotation.gtf.gz |sed -e '1,5d' - \
-  | awk -F '[";\t]' '{if($3=="gene"){print $1,$4,$5,$22,$10,$7}}' OFS="\t" > gencode.vM16.genes.bed
-  
-gtf=./gencode.vM16.annotation.gtf.gz
-gtf_bed=./gencode.vM16.genes.bed 
+gtf=${genomedir}/gencode.vM16.annotation.chr2.gtf
+gtf_bed=${genomedir}/gencode.vM16.genes.chr2.bed 
 
 mkdir RNAseq
 datadir=./RNAseq
@@ -18,6 +14,10 @@ datadir=./RNAseq
 # obtain sequences
 ./fethseq.sh
 
-$align ERR1950101 $genomedir $gtf $gtf_bed $datadir
-cd ERR1950101
-$chop ERR1950101 ERR1950101Aligned.sortedByCoord.out.bam chr2
+for i in ERR1950095 ERR1950098 ERR1950099 ERR1950101
+do
+$align ${i} $genomedir $gtf $gtf_bed $datadir
+cd ${i}
+$chop ${i} ${i}Aligned.sortedByCoord.out.bam chr2
+cd ..
+done

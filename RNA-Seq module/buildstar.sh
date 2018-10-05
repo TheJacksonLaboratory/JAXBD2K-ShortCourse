7bf60d30578be89a3bdc0750b47f2987b9fe8be7
Y. Ada Zhan <zyqfrog10@gmail.com>
2018-10-05 15:42:26 -0400

    Update buildstar.sh

--- RNA-Seq module/buildstar.sh
+++ RNA-Seq module/buildstar.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 mkdir -p mm10_star
-cd mm10
-gunzip gencode.vM16.annotation.gtf.gz
-cd ../mm10_star
-module load STAR/2.5.3
+cd mm10_star
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M16/gencode.vM16.annotation.gtf.gz
+gunzip gencode.vM16.annotation.gtf.gz |sed -e '1,5d' - |awk '{if($1=="chr2") print}' OFS="\t" > gencode.vM16.annotation.chr2.gtf
+awk -F '[";\t]' '{if($3=="gene"){print $1,$4,$5,$22,$10,$7}}' OFS="\t" gencode.vM16.annotation.chr2.gtf > gencode.vM16.genes.chr2.bed
+
+wget http://hgdownload.cse.ucsc.edu/goldenpath/mm10/chromosomes/chr2.fa.gz
+gunzip chr2.fa.gz && mv chr2.fa mm10.chr2.fa
+
+
+# module load STAR/2.5.3 (Require STAR)
 ### Build a genome index
-STAR --runThreadN 12 --runMode genomeGenerate --genomeDir /projects/zhany/genome/mm10_star \
---genomeFastaFiles /projects/zhany/genome/mm10/mm10.fa \
---sjdbGTFfile /projects/zhany/genome/mm10/gencode.vM16.annotation.gtf \
+STAR --runThreadN 12 --runMode genomeGenerate --genomeDir ./ \
+--genomeFastaFiles mm10.chr2.fa \
+--sjdbGTFfile gencode.vM16.annotation.chr2.gtf \
 --sjdbOverhang 100

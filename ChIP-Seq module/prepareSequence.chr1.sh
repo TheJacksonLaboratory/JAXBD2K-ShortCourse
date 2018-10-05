6213fb66164284fed1c58c80177ce1adb9035e92
Y. Ada Zhan <zyqfrog10@gmail.com>
2018-10-05 14:54:38 -0400

    Update prepareSequence.chr1.sh

--- ChIP-Seq module/prepareSequence.chr1.sh
+++ ChIP-Seq module/prepareSequence.chr1.sh
@@ -22,12 +22,12 @@ samtools view -@ 10 -bhS ${i}.sort chr1 |samtools sort -n - -o ${i}.chr1.bam
 bedtools bamtofastq -i ${i}.chr1.bam -fq ${i}.chr1.fq
 #rm ${i}.chr1.fq.gz
 #gzip ${i}.chr1.fq
-bowtie -m 1 -S ../hg38/GRCh38.chr1 ${i}.chr1.fq |samtools view -@ 10 -bhS - > ${i}.chr1.realigned.bam
-samtools sort ${i}.chr1.realigned.bam -o ${i}.chr1.realigned.sort.bam
-samtools index ${i}.chr1.realigned.sort.bam
+#bowtie -m 1 -S ../hg38/GRCh38.chr1 ${i}.chr1.fq |samtools view -@ 10 -bhS - > ${i}.chr1.realigned.bam
+#samtools sort ${i}.chr1.realigned.bam -o ${i}.chr1.realigned.sort.bam
+#samtools index ${i}.chr1.realigned.sort.bam
 
-samtools sort ${i}.chr1.bam -o ${i}.chr1.sort.bam
-samtools index ${i}.chr1.sort.bam
+#samtools sort ${i}.chr1.bam -o ${i}.chr1.sort.bam
+#samtools index ${i}.chr1.sort.bam
 done
 
 mv ENCFF834CAH.chr1.fq GM12878_CTCF_chr1.fastq
